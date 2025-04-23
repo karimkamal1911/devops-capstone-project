22da09f49acb2e0a75b02e08fa56015732b23f3e
karimkamal1911 <keko.amr.kamal@gmail.com>
2025-04-23 15:41:04 -0400

    Updating

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -146,4 +146,5 @@ class TestAccountService(TestCase):
             content_type="test/html"
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
-        
+
+    # ADD YOUR TEST CASES HERE ...
